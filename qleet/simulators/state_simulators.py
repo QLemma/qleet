b5d0930aa3524ff5a8e5e1b6c8930cecdc76ddfb
Animesh Sinha <AnimeshSinha1309@gmail.com>
2021-09-21 18:56:06 +0530

    State simulation available on the cirq backend
    Simple state vector and density matrix simulations for cirq.

--- qleet/simulators/state_simulators.py
+++ qleet/simulators/state_simulators.py
@@ -1,5 +1,6 @@
 import typing
 
+import cirq
 import numpy as np
 import qiskit
 
@@ -12,6 +13,12 @@ class StateSimulator:
         circuit: qleet.utils.circuit.CircuitDescriptor,
         noise_model: typing.Optional[dict] = None,
     ):
+        """
+        Initialize the state simulator
+        :param circuit: the target circuit to simulate
+        :param noise_model: the noise model as dict or empty dict for density matrix simulations,
+            None if performing state vector simulations
+        """
         self.circuit = circuit
         self.noise_model = noise_model
 
@@ -20,10 +27,14 @@ class StateSimulator:
         param_resolver: typing.Dict[qiskit.circuit.Parameter, float],
         shots: int = 1024,
     ) -> np.ndarray:
+        """
+        Simulate to get the state vector or the density matrix
+        :param param_resolver: a dictionary of all the symbols/parameters mapping to their values
+        :param shots: number of times to run the qiskit density matrix simulator
+        :returns: state vector or density matrix resulting from the simulation
+        """
         if self.circuit.default_backend == "qiskit":
-            circuit = qiskit.QuantumCircuit.copy(
-                self.circuit.qiskit_circuit
-            ).bind_parameters(param_resolver)
+            circuit = self.circuit.qiskit_circuit.bind_parameters(param_resolver)
             if self.noise_model is not None:
                 circuit.snapshot("final", snapshot_type="density_matrix")
                 result = qiskit.execute(
@@ -43,5 +54,12 @@ class StateSimulator:
                 ).result()
                 result_data = result.data(0)["snapshots"]["statevector"]["final"][0]
             return result_data
-        if self.circuit.default_backend == "cirq":
-            raise NotImplementedError("Simulator for ")
+        elif self.circuit.default_backend == "cirq":
+            simulator = cirq.Simulator()
+            result = simulator.simulate(self.circuit.cirq_circuit, param_resolver)
+            if self.noise_model is None:
+                return result.final_state_vector
+            else:
+                return result.density_matrix_of()
+        else:
+            raise NotImplementedError("Parametrized circuit simulation is not implemented for this backend.")
--- tests/simulators/test_state_simulators.py
+++ tests/simulators/test_state_simulators.py
@@ -6,7 +6,7 @@ import qiskit
 import qleet
 
 
-def test_cirq_simulator():
+def test_cirq_simulator_state_vector():
     params = sympy.symbols("param:%d" % 2)
     cirq_circuit = cirq.Circuit(
         [
@@ -19,6 +19,33 @@ def test_cirq_simulator():
         circuit=cirq_circuit, params=params, cost_function=cirq.PauliSum()
     )
     params = {p: np.random.random() * 2 * np.pi for p in cirq_descriptor.parameters}
+    simulator = qleet.simulators.state_simulators.StateSimulator(cirq_descriptor)
+    state_vector = simulator.simulate(params)
+    assert isinstance(state_vector, np.ndarray), "State vector should be a numpy array"
+    assert (
+        len(state_vector.shape) == 1 and state_vector.shape[0] == 4
+    ), "State vector is not of right shape"
+
+
+def test_cirq_simulator_density_matrix():
+    params = sympy.symbols("param:%d" % 2)
+    cirq_circuit = cirq.Circuit(
+        [
+            cirq.rx(params[0]).on(cirq.NamedQubit("q_0")),
+            cirq.CX(cirq.NamedQubit("q_0"), cirq.NamedQubit("q_1")),
+            cirq.rx(params[1]).on(cirq.NamedQubit("q_1")),
+        ]
+    )
+    cirq_descriptor = qleet.utils.circuit.CircuitDescriptor(
+        circuit=cirq_circuit, params=params, cost_function=cirq.PauliSum()
+    )
+    params = {p: np.random.random() * 2 * np.pi for p in cirq_descriptor.parameters}
+    simulator = qleet.simulators.state_simulators.StateSimulator(cirq_descriptor, noise_model=dict())
+    density_matrix = simulator.simulate(params)
+    assert isinstance(density_matrix, np.ndarray), "Density matrix should be a numpy array"
+    assert (
+        len(density_matrix.shape) == 2 and density_matrix.shape[0] == 4 and density_matrix.shape[1] == 4
+    ), "State vector is not of right shape"
 
 
 def test_qiskit_simulator():
